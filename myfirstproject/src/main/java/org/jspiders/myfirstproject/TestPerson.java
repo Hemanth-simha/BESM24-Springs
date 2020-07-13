abf5ae2c021047c819ea61254910510662716503
Hemanth-simha <58566844+Hemanth-simha@users.noreply.github.com>
2020-07-13 20:16:04 +0530

    Primitive Type

---
+++ myfirstproject/src/main/java/org/jspiders/myfirstproject/TestPerson.java
@@ -0,0 +1,20 @@
+package org.jspiders.myfirstproject;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = null;
+		context = new ClassPathXmlApplicationContext("org/jspiders/myfirstproject/config.xml");
+		
+		Person person = (Person) context.getBean("person");
+		System.out.println(person);
+		
+		if(context!=null) {
+			context.close();
+		}
+	}
+
+}
